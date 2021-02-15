24fb4997909f4e7effb91e8e7831743d676e260c
Laurent LELION <l.lelion@callmedicall.com>
2021-02-15 16:14:27 +0100

    added router && views

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import styled from 'styled-components'
+
+// -- Components
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+
+// -- Views
+import HomeView from './views/HomeView';
+import NoMatchView from './views/NoMatchView';
 
 const Wrapper = styled.div`
   margin: 40px auto;
@@ -7,9 +15,33 @@ const Wrapper = styled.div`
 `
 
 const App = () => (
-  <Wrapper>
-    Hi !
-  </Wrapper>
+
+    <Router>
+
+        <div className="App">
+
+            <Switch>
+
+                <Route exact path="/">
+                    <ErrorBoundary>
+                        <Wrapper>
+                            Hi !
+                            {/*<HomeView/>*/}
+                        </Wrapper>
+                    </ErrorBoundary>
+                </Route>
+
+                <Route path="*">
+                    <ErrorBoundary>
+                        <NoMatchView/>
+                    </ErrorBoundary>
+                </Route>
+
+            </Switch>
+
+        </div>
+
+    </Router>
 )
 
 export default App;
---
+++ src/helpers/helpers.js
@@ -0,0 +1,4 @@
+
+
+const FILE_NAME = 'helpers';
+
